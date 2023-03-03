72866b52d076d172cbaeaeecb819d34c07e91e2c
shamsartem <shamsartem@gmail.com>
2023-03-03 12:28:24 +0100

    feat: update copyright year (#200)

--- bin/dev.js
+++ bin/dev.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm/transpile-only
 
 /**
- * Copyright 2022 Fluence Labs Limited
+ * Copyright 2023 Fluence Labs Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
