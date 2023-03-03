bbfa34fdd46d21397ae3836e6a6b48b60595c91e
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2023-03-03 12:24:09 +0100

    feat: update copyright year

--- bin/run.js
+++ bin/run.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /**
- * Copyright 2022 Fluence Labs Limited
+ * Copyright 2023 Fluence Labs Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
