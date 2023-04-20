cfcc34248d0eea1963a9babd08b1a7ce390be719
Anatoly Laskaris <github_me@nahsi.dev>
2023-04-20 11:53:23 +0300

    Rebuild

--- .github/actions/replace-version/dist/index.js
+++ .github/actions/replace-version/dist/index.js
@@ -2838,19 +2838,29 @@ const path = __nccwpck_require__(17);
 
 async function run() {
   try {
-    const inputVersions = JSON.parse(core.getInput('versions'));
+    const inputVersions = JSON.parse(core.getInput("versions"));
 
-    const versionsFilePath = path.join(process.env.GITHUB_WORKSPACE, 'src', 'src/versions.json');
-    const versionsFileContent = fs.readFileSync(versionsFilePath, 'utf-8');
+    const versionsFilePath = path.join(
+      process.env.GITHUB_WORKSPACE,
+      "src",
+      "versions.json"
+    );
+    const versionsFileContent = fs.readFileSync(versionsFilePath, "utf-8");
     const versions = JSON.parse(versionsFileContent);
 
     let cargoDependencyUpdated = false;
 
     // Merge inputVersions into versions
     for (const category in inputVersions) {
-      if (versions.hasOwnProperty(category) && inputVersions[category] != null) {
+      if (
+        versions.hasOwnProperty(category) &&
+        inputVersions[category] != null
+      ) {
         for (const component in inputVersions[category]) {
-          if (versions[category].hasOwnProperty(component) && inputVersions[category][component] !== 'null') {
+          if (
+            versions[category].hasOwnProperty(component) &&
+            inputVersions[category][component] !== "null"
+          ) {
             versions[category][component] = inputVersions[category][component];
 
             // Check if a cargo dependency was updated
@@ -2873,7 +2883,6 @@ async function run() {
     if (cargoDependencyUpdated) {
       core.exportVariable("CARGO_REGISTRY_DEFAULT", "fluence");
     }
-
   } catch (error) {
     core.setFailed(error.message);
   }
