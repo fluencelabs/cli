9c512df5a313a33446f3919d695ac1eb9554da0a
Anatolios Laskaris <github_me@nahsi.dev>
2023-04-20 16:01:23 +0300

    chore: Update replace-const action to use versions.json (#246)
    * Update replace-const action to use versions.json
    
    * Update
    
    * Fix
    
    * Rebuild
    
    * Add CI variable

---
+++ .github/actions/replace-version/index.js
@@ -0,0 +1,68 @@
+// @ts-check
+import { exportVariable, getInput, setFailed } from "@actions/core";
+import assert from "assert";
+import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+
+try {
+  const inputVersions = JSON.parse(getInput("versions"));
+  assert(typeof inputVersions === "object" && inputVersions !== null);
+
+  assert(
+    typeof process.env.GITHUB_WORKSPACE === "string",
+    "GITHUB_WORKSPACE environment variable is not set"
+  );
+
+  const versionsFilePath = join(
+    process.env.GITHUB_WORKSPACE,
+    "src",
+    "versions.json"
+  );
+
+  const versionsFileContent = readFileSync(versionsFilePath, "utf-8");
+  const versions = JSON.parse(versionsFileContent);
+  assert(typeof versions === "object" && versions !== null);
+
+  let cargoDependencyUpdated = false;
+
+  // Merge inputVersions into versions
+  for (const category in inputVersions) {
+    if (
+      !versions.hasOwnProperty(category) ||
+      inputVersions[category] === null
+    ) {
+      continue;
+    }
+
+    for (const component in inputVersions[category]) {
+      if (
+        !versions[category].hasOwnProperty(component) ||
+        inputVersions[category][component] === "null"
+      ) {
+        continue;
+      }
+
+      versions[category][component] = inputVersions[category][component];
+
+      // Check if a cargo dependency was updated
+      if (category === "cargo") {
+        cargoDependencyUpdated = true;
+      }
+    }
+  }
+
+  const newVersionsJSONString = JSON.stringify(versions, null, 2);
+
+  // Save updated versions.json
+  writeFileSync(versionsFilePath, newVersionsJSONString);
+
+  // Print updated versions.json to stdout
+  console.log(`Updated versions.json:\n${newVersionsJSONString}`);
+
+  // Set CARGO_REGISTRY_DEFAULT if cargoDependencyUpdated is true
+  if (cargoDependencyUpdated) {
+    exportVariable("CARGO_REGISTRY_DEFAULT", "fluence");
+  }
+} catch (error) {
+  setFailed(error.message);
+}
