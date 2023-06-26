32c82f3ad518455ad18c114a147711854113810c
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2023-06-26 15:31:29 +0200

    feat: automatically add spell to fluence.yaml upon creation [fixes DXJ-395]

--- src/commands/spell/new.ts
+++ src/commands/spell/new.ts
@@ -14,24 +14,26 @@
  * limitations under the License.
  */
 
+import assert from "assert";
 import { mkdir, writeFile } from "fs/promises";
-import { join } from "path";
+import { join, relative } from "path";
 
 import oclifColor from "@oclif/color";
 const color = oclifColor.default;
 import { Args, Flags } from "@oclif/core";
 
 import { BaseCommand, baseFlags } from "../../baseCommand.js";
-import { commandObj } from "../../lib/commandObj.js";
+import { commandObj, isInteractive } from "../../lib/commandObj.js";
 import { initNewReadonlySpellConfig } from "../../lib/configs/project/spell.js";
 import {
+  DEFAULT_WORKER_NAME,
   FS_OPTIONS,
   SPELL_AQUA_FILE_CONTENT,
   SPELL_AQUA_FILE_NAME,
 } from "../../lib/const.js";
 import { initCli } from "../../lib/lifeCycle.js";
 import { ensureSrcSpellsDir } from "../../lib/paths.js";
-import { input } from "../../lib/prompt.js";
+import { confirm, input } from "../../lib/prompt.js";
 
 export default class New extends BaseCommand<typeof New> {
   static override description = "Create a new spell template";
@@ -49,7 +51,10 @@ export default class New extends BaseCommand<typeof New> {
     }),
   };
   async run(): Promise<void> {
-    const { args } = await initCli(this, await this.parse(New));
+    const { args, maybeFluenceConfig } = await initCli(
+      this,
+      await this.parse(New)
+    );
 
     const spellName =
       args.name ?? (await input({ message: "Enter spell name" }));
@@ -62,6 +67,54 @@ export default class New extends BaseCommand<typeof New> {
       `Successfully generated template for new spell at ${color.yellow(
         pathToSpellDir
       )}`
+    );
+
+    if (maybeFluenceConfig === null) {
+      return;
+    }
+
+    const fluenceConfig = maybeFluenceConfig;
+
+    if (fluenceConfig.spells === undefined) {
+      fluenceConfig.spells = {};
+    }
+
+    fluenceConfig.spells[spellName] = {
+      get: relative(fluenceConfig.$getDirPath(), pathToSpellDir),
+    };
+
+    await fluenceConfig.$commit();
+
+    if (
+      !(
+        isInteractive &&
+        fluenceConfig.workers !== undefined &&
+        DEFAULT_WORKER_NAME in fluenceConfig.workers &&
+        !(fluenceConfig.workers[DEFAULT_WORKER_NAME]?.spells ?? []).includes(
+          spellName
+        ) &&
+        (await confirm({
+          message: `Do you want to add spell ${color.yellow(
+            spellName
+          )} to a default worker ${color.yellow(DEFAULT_WORKER_NAME)}`,
+        }))
+      )
+    ) {
+      return;
+    }
+
+    const defaultWorker = fluenceConfig.workers[DEFAULT_WORKER_NAME];
+    assert(defaultWorker !== undefined);
+
+    fluenceConfig.workers[DEFAULT_WORKER_NAME] = {
+      ...defaultWorker,
+      spells: [...(defaultWorker.spells ?? []), spellName],
+    };
+
+    await fluenceConfig.$commit();
+
+    commandObj.log(
+      `Added ${color.yellow(spellName)} to ${color.yellow(DEFAULT_WORKER_NAME)}`
     );
   }
 }
