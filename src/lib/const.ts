32dc514406f738b09ee30b7a31cec1eeee50b1f0
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2023-08-30 21:47:46 +0200

    switch

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -44,7 +44,7 @@ export const DEFAULT_MARINE_BUILD_ARGS = `--release`;
 export const U32_MAX = 4_294_967_295;
 export const CHECK_FOR_UPDATES_INTERVAL = 1000 * 60 * 60 * 24; // 1 day
 
-export const CONTRACTS_ENV = ["local", "testnet"] as const;
+export const CONTRACTS_ENV = ["testnet", "local"] as const;
 export type ContractsENV = (typeof CONTRACTS_ENV)[number];
 
 export const DEFAULT_CHAIN_NETWORK = CONTRACTS_ENV[1];
