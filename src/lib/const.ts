4b41c773587918e23684e94b713d4b84a99fd553
elshan_eth <raiman.elshan@gmail.com>
2023-10-05 18:11:57 +0200

    fix: change rpc

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -68,15 +68,15 @@ export const isFluenceEnv = getIsStringUnion(FLUENCE_ENVS);
 
 export const DEAL_CONFIG: Record<ContractsENV, ChainConfig> = {
   kras: {
-    url: "https://rpc-mumbai.maticvigil.com",
+    url: "https://kras-rpc.fluence.dev",
     id: 80001,
   },
   testnet: {
-    url: "https://rpc-mumbai.maticvigil.com",
+    url: "https://testnet-rpc.fluence.dev",
     id: 80001,
   },
   stage: {
-    url: "https://rpc-mumbai.maticvigil.com",
+    url: "https://stage-rpc.fluence.dev",
     id: 80001,
   },
   local: {
