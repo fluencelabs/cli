b3d9796ff788f9d21e92a4ddbeb0a8484225ae70
Anatoly Laskaris <github_me@nahsi.dev>
2023-10-10 19:03:02 +0300

    Force symlink

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ tar --strip-components=1 -xzf "${TEMP}/${archive}" -C "${FLUENCE_USER_DIR}/cli"
 
 echo "Fluence CLI installation complete!"
 if echo $PATH | grep -q $HOME/.local/bin ; then
-  ln -s ${FLUENCE_USER_DIR}/cli/bin/fluence $HOME/.local/bin/fluence
+  ln -sf ${FLUENCE_USER_DIR}/cli/bin/fluence $HOME/.local/bin/fluence
 else
   echo "Create a symlink to fluence binary in any directory that is in \$PATH, for example:"
   echo "sudo ln -s ${FLUENCE_USER_DIR}/cli/bin/fluence /usr/local/bin/fluence"
