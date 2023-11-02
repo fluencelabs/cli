ec2f767a2f5cc8c7113d8d82982143eda2b3607c
Anatoly Laskaris <github_me@nahsi.dev>
2023-11-02 17:07:08 +0200

    Check that wget is installed

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+has() {
+  [[ -z $1 ]] && return 1
+  command -v $1 >/dev/null 2>&1
+}
+
 echo "Starting Fluence installation..."
 
 # Identify OS and architecture
@@ -14,24 +19,39 @@ FLUENCE_USER_DIR="${FLUENCE_USER_DIR-$HOME/.fluence}"
 TEMP="$(mktemp -d)"
 trap "rm -rf '$TEMP'" EXIT INT TERM
 
+# Check that wget is installed
+if ! has "wget"; then
+  echo "'wget' is required for the installation. Please install it and rerun the script"
+  exit 1
+fi
+
 # Verify if the cli directory is empty
 if $(ls -1 ${FLUENCE_USER_DIR}/cli &>/dev/null); then
-  echo "Error: ${FLUENCE_USER_DIR}/cli exists and is not empty. Please remove it and rerun the script."
+  cat <<ERR
+Error: ${FLUENCE_USER_DIR}/cli exists and is not empty.
+Remove with rm -rf ${FLUENCE_USER_DIR}/cli and rerun the script.
+ERR
   exit 1
 fi
 
 # Validate and set system architecture
 case "$ARCH" in
-  "x86_64") arch="x64";;
-  "arm64" | "aarch64") arch="arm64";;
-  *) echo "Error: Unsupported architecture - $ARCH"; exit 1;;
+"x86_64") arch="x64" ;;
+"arm64" | "aarch64") arch="arm64" ;;
+*)
+  echo "Error: Unsupported architecture - $ARCH"
+  exit 1
+  ;;
 esac
 
 # Validate and set the operating system type
 case "$OS" in
-  "darwin") os="darwin";;
-  "linux") os="linux";;
-  *) echo "Error: Unsupported OS - $OS"; exit 1;;
+"darwin") os="darwin" ;;
+"linux") os="linux" ;;
+*)
+  echo "Error: Unsupported OS - $OS"
+  exit 1
+  ;;
 esac
 
 # Set archive name and URL based on OS and architecture
@@ -45,7 +65,7 @@ wget -q --show-progress -O "${TEMP}/${archive}" "$url"
 tar --strip-components=1 -xzf "${TEMP}/${archive}" -C "${FLUENCE_USER_DIR}/cli"
 
 echo "Fluence CLI installation complete!"
-if echo $PATH | grep -q $HOME/.local/bin ; then
+if echo $PATH | grep -q $HOME/.local/bin; then
   mkdir -p $HOME/.local/bin
   ln -sf ${FLUENCE_USER_DIR}/cli/bin/fluence $HOME/.local/bin/fluence
 else
