b172e7eec8deeb33225c835fc0d80432b3927a10
akim-bow <akim-bow@users.noreply.github.com>
2023-11-17 20:05:08 +0000

    Apply automatic changes

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -408,7 +408,11 @@ const fluenceRun = async (args: RunArgs) => {
   const { Fluence, v5_callFunction } = await import("@fluencelabs/js-client");
 
   const runArgs = Object.values(args.runData ?? []);
-  const result = await v5_callFunction([Fluence.getClient(), ...runArgs], functionCall.funcDef,  functionCall.script);
+  const result = await v5_callFunction(
+    [Fluence.getClient(), ...runArgs],
+    functionCall.funcDef,
+    functionCall.script,
+  );
 
   return result;
 };
