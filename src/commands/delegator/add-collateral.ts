534f629cc2e5695a97842b6cb1ba9b89cde67774
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2024-02-05 12:14:36 +0100

    add delegator commands

---
+++ src/commands/delegator/add-collateral.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2023 Fluence Labs Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Args } from "@oclif/core";
+
+import { BaseCommand, baseFlags } from "../../baseCommand.js";
+import { depositCollateral } from "../../lib/chain/depositCollateral.js";
+import { PRIV_KEY_FLAG, ENV_FLAG } from "../../lib/const.js";
+import { commaSepStrToArr } from "../../lib/helpers/utils.js";
+import { initCli } from "../../lib/lifeCycle.js";
+import { input } from "../../lib/prompt.js";
+
+export default class AddCollateral extends BaseCommand<typeof AddCollateral> {
+  static override description = "Add collateral to capacity commitment";
+  static override aliases = ["delegator:ac"];
+  static override flags = {
+    ...baseFlags,
+    ...PRIV_KEY_FLAG,
+    ...ENV_FLAG,
+  };
+  static override args = {
+    IDS: Args.string({
+      description: "Comma separated capacity commitment IDs",
+    }),
+  };
+
+  async run(): Promise<void> {
+    const { args } = await initCli(this, await this.parse(AddCollateral));
+
+    await depositCollateral(
+      commaSepStrToArr(
+        args.IDS ??
+          (await input({
+            message: "Enter comma-separated capacity commitment IDs",
+          })),
+      ),
+    );
+  }
+}
