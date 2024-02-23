0b258282ef7c062855772207b65656e73e6354e9
Akim Mamedov <akim99999999@gmail.com>
2024-02-23 15:28:52 +0700

    Revert "Fix build"
    This reverts commit 12f51bdfe39e5596529d99e3212362561efced26.

--- src/beforeBuild.ts
+++ src/beforeBuild.ts
@@ -153,14 +153,6 @@ const WIN_BIN_FILE_PATH = join(
   "win.js",
 );
 
-const TARBALL_BUILD_COMMAND_PATH = join(
-  NODE_MODULES_DIR_NAME,
-  "oclif",
-  "lib",
-  "tarballs",
-  "build.js",
-);
-
 async function patchOclif(fileName: string, search: string, insert: string) {
   try {
     const binFileContent = await readFile(fileName, FS_OPTIONS);
@@ -194,13 +186,6 @@ await patchOclif(
   WIN_BIN_FILE_PATH,
   "setlocal enableextensions",
   "setlocal enableextensions\nset NODE_NO_WARNINGS=1",
-);
-
-// Packing replacement to fix build on Windows
-await patchOclif(
-  TARBALL_BUILD_COMMAND_PATH,
-  "tar -xzf",
-  "tar --force-local -xzf",
 );
 
 // Pack tar.gz for Windows. We need it to perform update
