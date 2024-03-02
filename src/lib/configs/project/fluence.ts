e3e83711e18ed1956a2d907c02918f6b20843f9e
Aleksey Proshutinskiy <justprosh@users.noreply.github.com>
2024-03-02 15:13:01 +0200

    feat: update deal-ts-clients; change defaults (#827)

--- src/lib/configs/project/fluence.ts
+++ src/lib/configs/project/fluence.ts
@@ -59,7 +59,6 @@ import {
   type AquaLogLevel,
   PT_SYMBOL,
   COMPILE_AQUA_PROPERTY_NAME,
-  DEFAULT_CURL_EFFECTOR_CID,
 } from "../../const.js";
 import { splitErrorsAndResults } from "../../helpers/utils.js";
 import {
@@ -950,8 +949,6 @@ deployments:
     initialBalance: "${DEFAULT_INITIAL_BALANCE}" # initial balance  after deploy in ${PT_SYMBOL}
     services: [] # list of service names to be deployed to this worker
     spells: [] # list of spell names to be deployed to this worker
-    effectors:
-      - "${DEFAULT_CURL_EFFECTOR_CID}"
 
 ${yamlDiffPatch(
   "",
