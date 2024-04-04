c2877dfe074284eef0438dcfcd6cda89d9b3e923
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2024-04-04 17:08:59 +0300

    fix

--- src/lib/dealClient.ts
+++ src/lib/dealClient.ts
@@ -340,6 +340,7 @@ export async function signBatch<T extends Array<unknown>>(
   return receipts;
 }
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 function methodCallToString([method, ...args]: [
   { name: string },
   ...unknown[],
