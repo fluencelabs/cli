20e780968922164ed45b5feeb9e5bca40ddb2e1d
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2024-04-16 15:58:03 +0200

    feat: improve offer-update logs [fixes DXJ-760]

--- src/lib/chain/offer.ts
+++ src/lib/chain/offer.ts
@@ -368,7 +368,7 @@ export async function updateOffers(flags: OffersArgs) {
 
     if (offerInfo.paymentToken !== usdcAddress) {
       populatedTxs.push({
-        description: `changing payment token from ${color.yellow(
+        description: `\nchanging payment token from ${color.yellow(
           offerInfo.paymentToken,
         )} to ${color.yellow(usdcAddress)}`,
         tx: [market.changePaymentToken, offerId, usdcAddress],
@@ -377,7 +377,7 @@ export async function updateOffers(flags: OffersArgs) {
 
     if (offerInfo.minPricePerWorkerEpoch !== minPricePerWorkerEpochBigInt) {
       populatedTxs.push({
-        description: `changing minPricePerWorker from ${color.yellow(
+        description: `\nchanging minPricePerWorker from ${color.yellow(
           await ptFormatWithSymbol(offerInfo.minPricePerWorkerEpoch),
         )} to ${color.yellow(
           await ptFormatWithSymbol(minPricePerWorkerEpochBigInt),
@@ -402,7 +402,7 @@ export async function updateOffers(flags: OffersArgs) {
 
     if (removedEffectors.length > 0) {
       populatedTxs.push({
-        description: `Removing effectors: ${removedEffectors.join(", ")}`,
+        description: `\nRemoving effectors:\n${removedEffectors.join("\n")}`,
         tx: [
           market.removeEffector,
           offerId,
@@ -423,7 +423,7 @@ export async function updateOffers(flags: OffersArgs) {
 
     if (addedEffectors.length > 0) {
       populatedTxs.push({
-        description: `Adding effectors: ${addedEffectors.join(", ")}`,
+        description: `\nAdding effectors:\n${addedEffectors.join("\n")}`,
         tx: [
           market.addEffector,
           offerId,
@@ -455,17 +455,22 @@ export async function updateOffers(flags: OffersArgs) {
         );
 
         if (alreadyRegisteredPeer === undefined) {
-          return computeUnits.map(({ id }) => {
-            return id;
-          });
+          return [];
         }
 
         if (alreadyRegisteredPeer.unitIds.length < computeUnits.length) {
-          return computeUnits
-            .slice(alreadyRegisteredPeer.unitIds.length - computeUnits.length)
-            .map(({ id }) => {
-              return id;
-            });
+          return [
+            {
+              peerIdBase58,
+              computeUnits: computeUnits
+                .slice(
+                  alreadyRegisteredPeer.unitIds.length - computeUnits.length,
+                )
+                .map(({ id }) => {
+                  return id;
+                }),
+            },
+          ];
         }
 
         return [];
@@ -474,11 +479,16 @@ export async function updateOffers(flags: OffersArgs) {
 
     if (computeUnitsToRemove.length > 0) {
       populatedTxs.push(
-        ...computeUnitsToRemove.map((computeUnit) => {
-          return {
-            description: `Removing compute unit: ${computeUnit}`,
-            tx: [market.removeComputeUnit, computeUnit],
-          };
+        ...computeUnitsToRemove.flatMap(({ peerIdBase58, computeUnits }) => {
+          return computeUnits.map((computeUnit, index) => {
+            return {
+              description:
+                index === 0
+                  ? `\nRemoving compute units from peer ${peerIdBase58}:\n${computeUnit}`
+                  : computeUnit,
+              tx: [market.removeComputeUnit, computeUnit],
+            };
+          });
         }),
       );
     }
@@ -491,12 +501,25 @@ export async function updateOffers(flags: OffersArgs) {
 
     if (computePeersToRemove.length > 0) {
       populatedTxs.push(
-        ...computePeersToRemove.map(({ peerIdBase58, hexPeerId }) => {
-          return {
-            description: `Removing peer: ${peerIdBase58}`,
-            tx: [market.removeComputePeer, hexPeerId],
-          };
-        }),
+        ...computePeersToRemove.flatMap(
+          ({ peerIdBase58, hexPeerId, computeUnits }) => {
+            return [
+              ...computeUnits.map((computeUnit, index) => {
+                return {
+                  description:
+                    index === 0
+                      ? `\nRemoving peer ${peerIdBase58} with compute units:\n${computeUnit.id}`
+                      : computeUnit.id,
+                  tx: [market.removeComputeUnit, computeUnit.id],
+                };
+              }),
+              {
+                description: "",
+                tx: [market.removeComputePeer, hexPeerId],
+              },
+            ];
+          },
+        ),
       );
     }
 
@@ -531,9 +554,9 @@ export async function updateOffers(flags: OffersArgs) {
       populatedTxs.push(
         ...computeUnitsToAdd.map(({ hexPeerId, unitIds, peerIdBase58 }) => {
           return {
-            description: `Adding ${numToStr(
-              unitIds.length,
-            )} compute units to peer id ${peerIdBase58}`,
+            description: `\nAdding compute units to peer ${peerIdBase58}:\n${unitIds.join(
+              "\n",
+            )}`,
             tx: [market.addComputeUnits, hexPeerId, unitIds],
           };
         }),
@@ -550,11 +573,11 @@ export async function updateOffers(flags: OffersArgs) {
 
     if (computePeersToAdd.length > 0) {
       populatedTxs.push({
-        description: `Adding peers:\n${computePeersToAdd
+        description: computePeersToAdd
           .map(({ peerIdBase58, unitIds }) => {
-            return `Peer: ${peerIdBase58} with ${numToStr(unitIds.length)} compute units`;
+            return `\nAdding peer ${peerIdBase58} with compute units:\n${unitIds.join("\n")}`;
           })
-          .join("\n")}`,
+          .join("\n"),
         tx: [market.addComputePeers, offerId, computePeersToAdd],
       });
     }
@@ -573,6 +596,9 @@ export async function updateOffers(flags: OffersArgs) {
       `\nUpdating offer ${color.yellow(offerName)} with id ${color.yellow(
         offerId,
       )}:\n${populatedTxs
+        .filter(({ description }) => {
+          return description !== "";
+        })
         .map(({ description }) => {
           return description;
         })
