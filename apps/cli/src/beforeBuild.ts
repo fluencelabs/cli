113133023e00b708e034e22f8c6b062a95ef0ca7
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2024-05-22 13:56:57 +0200

    move before-build to a separate step, move patch oclif to a separate script

--- apps/cli/src/beforeBuild.ts
+++ apps/cli/src/beforeBuild.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { cp, mkdir, readFile, rm, writeFile } from "node:fs/promises";
+import { cp, mkdir, rm, writeFile } from "node:fs/promises";
 import { join, resolve } from "node:path";
 
 import { compileFromPath } from "@fluencelabs/aqua-api";
@@ -145,66 +145,3 @@ await mkdir(COMPILED_INSTALLATION_SPELL_AQUA_WITH_TRACING_PATH, {
 
 await compileInstallationSpellAqua();
 await compileInstallationSpellAqua(true);
-
-const BIN_FILE_PATH = join(
-  join(WORKSPACE_NODE_MODULES_PATH, "oclif", "lib", "tarballs"),
-  "bin.js",
-);
-
-const WIN_BIN_FILE_PATH = join(
-  WORKSPACE_NODE_MODULES_PATH,
-  "oclif",
-  "lib",
-  "commands",
-  "pack",
-  "win.js",
-);
-
-async function patchOclif(fileName: string, search: string, insert: string) {
-  try {
-    const binFileContent = await readFile(fileName, FS_OPTIONS);
-
-    const hasSearch = binFileContent.includes(search);
-    const hasInsert = binFileContent.includes(insert);
-
-    if (hasSearch && !hasInsert) {
-      const newBinFileContent = binFileContent.replace(search, insert);
-
-      await writeFile(fileName, newBinFileContent, FS_OPTIONS);
-    } else if (!hasSearch && !hasInsert) {
-      throw new Error(`Wasn't able to find '${search}' in ${fileName}`);
-    }
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(`Error while modifying ${fileName}`);
-    throw err;
-  }
-}
-
-// Unix replacement
-await patchOclif(
-  BIN_FILE_PATH,
-  "#!/usr/bin/env bash",
-  "#!/usr/bin/env bash\nexport NODE_NO_WARNINGS=1",
-);
-
-// Windows replacement
-await patchOclif(
-  WIN_BIN_FILE_PATH,
-  "setlocal enableextensions",
-  "setlocal enableextensions\nset NODE_NO_WARNINGS=1",
-);
-
-// Pack tar.gz for Windows. We need it to perform update
-await patchOclif(
-  WIN_BIN_FILE_PATH,
-  "await Tarballs.build(buildConfig, { pack: false, parallel: true, platform: 'win32', tarball: flags.tarball });",
-  "await Tarballs.build(buildConfig, { pack: true, parallel: true, platform: 'win32', tarball: flags.tarball });",
-);
-
-// Set correct redirection command on Windows
-await patchOclif(
-  WIN_BIN_FILE_PATH,
-  '"%~dp0\\\\..\\\\client\\\\bin\\\\node.exe" "%~dp0\\\\..\\\\client\\\\${additionalCLI ? `${additionalCLI}\\\\bin\\\\run` : \'bin\\\\run\'}" %*',
-  '"%~dp0\\\\..\\\\client\\\\bin\\\\fluence.cmd" %*',
-);
