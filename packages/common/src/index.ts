d49a48c65a0668d2b17900516f298931f5b90291
folex <0xdxdy@gmail.com>
2024-07-26 17:35:59 +0300

    fix(deps): deal-ts-clients 0.15.1, diamond on Dar (#987)
    * fix(deps): deal-ts-clients 0.15.1
    
    * fix(dar,stage): update Dar rpc, stage ipfs

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -19,7 +19,7 @@ import "@total-typescript/ts-reset";
 import type { TransactionRequest } from "ethers";
 
 export const CHAIN_IDS = {
-  dar: 2358716091832359,
+  dar: 52164803,
   stage: 123420000220,
   kras: 1622562509754216,
   local: 31337,
@@ -45,7 +45,7 @@ export const isChainEnv = getIsUnion(CHAIN_ENV);
 
 export const CHAIN_URLS_WITHOUT_LOCAL = {
   kras: "https://ipc.kras.fluence.dev",
-  dar: "https://ipc.dar.fluence.dev",
+  dar: "https://rpc.fluence-testnet.t.raas.gelato.cloud",
   stage: "https://rpc-123420000220.raas-testnet.gelato.digital",
 } as const satisfies Record<Exclude<ChainENV, "local">, string>;
 
@@ -56,7 +56,7 @@ export const CHAIN_URLS = {
 
 export const BLOCK_SCOUT_URLS = {
   kras: "https://blockscout.kras.fluence.dev/",
-  dar: "https://blockscout.dar.fluence.dev/",
+  dar: "https://blockscout.testnet.fluence.dev/",
   stage: "https://blockscout.stage.fluence.dev/",
 } as const satisfies Record<Exclude<ChainENV, "local">, string>;
 
