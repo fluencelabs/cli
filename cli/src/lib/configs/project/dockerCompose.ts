8efb7652ddde362de66c42afba9ea3b40b7c2dd6
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2024-08-21 17:31:59 +0200

    use specific container version

--- cli/src/lib/configs/project/dockerCompose.ts
+++ cli/src/lib/configs/project/dockerCompose.ts
@@ -200,7 +200,7 @@ async function genDockerCompose(): Promise<LatestConfig> {
     ),
     services: {
       [IPFS_CONTAINER_NAME]: {
-        image: "ipfs/kubo",
+        image: "ipfs/kubo:master-2024-05-16-4f94f36",
         ports: [`${IPFS_PORT}:${IPFS_PORT}`, "4001:4001"],
         environment: {
           IPFS_PROFILE: "server",
