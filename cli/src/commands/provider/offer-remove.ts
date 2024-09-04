200a17153acc76cd19f9bbe0f72cc992b0847b5c
shamsartem <shamsartem@gmail.com>
2024-09-04 17:23:21 +0200

    feat: add offer-remove command (#1018)
    * feat: add offer-remove command
    
    * Apply automatic changes
    
    * simplify
    
    ---------
    
    Co-authored-by: shamsartem <shamsartem@users.noreply.github.com>

---
+++ cli/src/commands/provider/offer-remove.ts
@@ -0,0 +1,36 @@
+/**
+ * Fluence CLI
+ * Copyright (C) 2024 Fluence DAO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { BaseCommand, baseFlags } from "../../baseCommand.js";
+import { removeOffers } from "../../lib/chain/offer/updateOffers.js";
+import { CHAIN_FLAGS, OFFER_FLAG } from "../../lib/const.js";
+import { initCli } from "../../lib/lifeCycle.js";
+
+export default class OfferRemove extends BaseCommand<typeof OfferRemove> {
+  static override aliases = ["provider:or"];
+  static override description = "Remove offers";
+  static override flags = {
+    ...baseFlags,
+    ...OFFER_FLAG,
+    ...CHAIN_FLAGS,
+  };
+
+  async run(): Promise<void> {
+    const { flags } = await initCli(this, await this.parse(OfferRemove));
+    await removeOffers(flags);
+  }
+}
