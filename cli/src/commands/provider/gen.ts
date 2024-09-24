de8cfe9798e41010d9679dbec1272339dfd0a82b
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2024-09-24 15:03:39 +0200

    feat: add --reset-secrets flag, improve config validation error messages [JS-812, JS-813]

--- cli/src/commands/provider/gen.ts
+++ cli/src/commands/provider/gen.ts
@@ -15,6 +15,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { rm } from "fs/promises";
+
+import { color } from "@oclif/color";
+import { Flags } from "@oclif/core";
+
 import { BaseCommand, baseFlags } from "../../baseCommand.js";
 import { commandObj } from "../../lib/commandObj.js";
 import { ensureComputerPeerConfigs } from "../../lib/configs/project/provider.js";
@@ -22,12 +27,16 @@ import {
   CHAIN_FLAGS,
   PROVIDER_CONFIG_FULL_FILE_NAME,
   PROVIDER_SECRETS_CONFIG_FULL_FILE_NAME,
+  DOT_FLUENCE_DIR_NAME,
+  SECRETS_DIR_NAME,
 } from "../../lib/const.js";
 import { initCli } from "../../lib/lifeCycle.js";
 import {
   getFluenceSecretsDir,
   ensureFluenceConfigsDir,
+  ensureProviderSecretsConfigPath,
 } from "../../lib/paths.js";
+import { confirm } from "../../lib/prompt.js";
 
 export default class Gen extends BaseCommand<typeof Gen> {
   static override description = `Generate Config.toml files according to ${PROVIDER_CONFIG_FULL_FILE_NAME} and secrets according to ${PROVIDER_SECRETS_CONFIG_FULL_FILE_NAME}`;
@@ -35,9 +44,27 @@ export default class Gen extends BaseCommand<typeof Gen> {
   static override flags = {
     ...baseFlags,
     ...CHAIN_FLAGS,
+    "reset-secrets": Flags.boolean({
+      description: `Remove nox secrets at ${DOT_FLUENCE_DIR_NAME}/${PROVIDER_SECRETS_CONFIG_FULL_FILE_NAME} and ${DOT_FLUENCE_DIR_NAME}/${SECRETS_DIR_NAME} to generate new ones`,
+      default: false,
+    }),
   };
   async run(): Promise<void> {
-    await initCli(this, await this.parse(Gen));
+    const { flags } = await initCli(this, await this.parse(Gen));
+    const providerSecretsConfigPath = await ensureProviderSecretsConfigPath();
+    const fluenceSecretsDir = getFluenceSecretsDir();
+
+    if (
+      flags["reset-secrets"] &&
+      (await confirm({
+        message: `Are you sure you want to remove ${color.yellow(providerSecretsConfigPath)} and ${color.yellow(fluenceSecretsDir)} to generate new ones`,
+        default: true,
+      }))
+    ) {
+      await rm(providerSecretsConfigPath, { recursive: true, force: true });
+      await rm(fluenceSecretsDir, { recursive: true, force: true });
+    }
+
     await ensureComputerPeerConfigs();
 
     commandObj.logToStderr(
